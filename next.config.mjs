ea3bc2f88ab5ec29efabd39fd0ced129e1c133e3
Essadike  Elhafiane <eelhafia@e1r8p6.1337.ma>
2024-02-10 21:12:15 +0100

    g

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 /**
  * @type {import('next').NextConfig}
  */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-    experimental: {
-        output: 'standalone',
-      },
-      basePath: isProd ? '/front' : '',
-      assetPrefix: isProd ? '/front/' : '',
-    // output: 'export',
+   
+    output: 'export',
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
