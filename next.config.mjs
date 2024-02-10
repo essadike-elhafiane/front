a90ba23803178603d84d2754dcc4a5139134b0c2
Essadike  Elhafiane <eelhafia@e1r8p6.1337.ma>
2024-02-10 19:47:22 +0100

    intra fix:

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    output: 'export',
+    images: {
+      loader: 'custom',
+      loaderFile: './my-loader.ts',
+    },
 };
 
 export default nextConfig;
