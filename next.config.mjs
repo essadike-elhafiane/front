d0fdf9804cb463c0e53f15aba7fa6ad86df86a7b
Essadike  Elhafiane <eelhafia@e1r8p6.1337.ma>
2024-02-10 20:17:06 +0100

    intra fix:

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
@@ -14,4 +14,4 @@ const nextConfig = {
     // distDir: 'dist',
   }
    
- module.exports = nextConfig
+ export default nextConfig
