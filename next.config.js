c4a51223cf7623d2354db7611a51e6076edde971
Essadike  Elhafiane <eelhafia@e1r8p6.1337.ma>
2024-02-13 00:00:05 +0100

    g

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@
  */
 const nextConfig = {
     output: 'export',
-    images: { unoptimized: true },
+    // images: { unoptimized: true },
+    swcMinify: true,
     // Other optional configurations...
   };
   
