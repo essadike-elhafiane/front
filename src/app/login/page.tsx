f40a759d1ac3242646d91c86f9825244f85e92e3
Essadike  Elhafiane <eelhafia@e1r8p6.1337.ma>
2024-02-13 02:56:24 +0100

    g

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,9 @@
 import Image from 'next/image';
 import '../../style/styles.css'
 // import '../../style/styles copy.css';
-import Signup from '../../components/sinup'
+import Signup from '@/components/sinup'
 import React, { useEffect, useState } from 'react';
-import Signin from '../../components/singin';
+import Signin from '@/components/singin';
 import BackGround from '@/components/bg';
 
 
@@ -39,35 +39,37 @@ function main()
 
         <>
         <BackGround/>
-        <div className='container' >
-            <div className='row'>
-              <h1 className='h1logo'>PO<span>ng</span>Master</h1>
+        <div className='main'>
+          <div className='container' >
+              <div className='row'>
+                <h1 className='h1logo'>PO<span>ng</span>Master</h1>
+                  
                 
-              
-              <div id='loginBtn'>
-                  <a href='#' className={`${insignin ? 'inactive_sing' : 'active_sing' }`} onClick={()=>{
-                    setsign(false)
-                  }}>Sign up</a>
+                <div id='loginBtn'>
+                    <a href='#' className={`${insignin ? 'inactive_sing' : 'active_sing' }`} onClick={()=>{
+                      setsign(false)
+                    }}>Sign up</a>
 
-                  <a href='#' className={`${insignin ? 'active_sing' : 'inactive_sing'}`} onClick={()=>{
-                    setsign(true);
-                  }}>Login</a>
+                    <a href='#' className={`${insignin ? 'active_sing' : 'inactive_sing'}`} onClick={()=>{
+                      setsign(true);
+                    }}>Login</a>
 
-                  {insignin? <Signin /> : <Signup/>}
-              </div>
-            </div>
-            <div className='row1'>
-                <div id='div-row1'>
-                    <div  className='btnAuth' onClick={authIntra}>
-                      <Image src="./intra.png" alt="" width={'100'} height={'100'}/>
-                      <span>Intra</span>
-                    </div>
-                    <div  className='btnAuth' onClick={authGoogle}>
-                      <Image src='./googlelogo.png' alt="" width={'100'} height={'100'}/>
-                      <span>Google</span>
-                    </div>
+                    {insignin? <Signin /> : <Signup/>}
                 </div>
-            </div>
+              </div>
+              <div className='row1'>
+                  <div id='div-row1'>
+                      <div  className='btnAuth' onClick={authIntra}>
+                        <Image src="./intra.png" alt="" width={'100'} height={'100'}/>
+                        <span>Intra</span>
+                      </div>
+                      <div  className='btnAuth' onClick={authGoogle}>
+                        <Image src='./googlelogo.png' alt="" width={'100'} height={'100'}/>
+                        <span>Google</span>
+                      </div>
+                  </div>
+              </div>
+          </div>
         </div>
         </>
     );
