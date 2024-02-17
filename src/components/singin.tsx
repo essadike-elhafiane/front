3354acdf73ffd73d72df95d8f9d8bb60c687edd2
Essadike  Elhafiane <eelhafia@e1r7p6.1337.ma>
2024-02-17 17:55:46 +0100

    g

--- src/components/singin.tsx
+++ src/components/singin.tsx
@@ -7,15 +7,16 @@ export default function Signin() {
     const router = useRouter();
 
     
-    function singRequest()
+    function singRequest(e: React.FormEvent<HTMLFormElement>)
     {
-
+        e.preventDefault();
         console.log('singRequest');
         const data = {
             userName: (document.getElementsByName("Email or Username")[0] as HTMLInputElement).value,
             email: (document.getElementsByName("Email or Username")[0] as HTMLInputElement).value,
             password: (document.getElementsByName("Password")[0] as HTMLInputElement).value
         }
+
         fetch('http://localhost:3000/singin', {
             method: 'POST',
             headers: {
@@ -30,20 +31,23 @@ export default function Signin() {
                 return setError(true);
             }
             router.push('/');
+            e.currentTarget.reset();
+            console.log('Success:', e.currentTarget);
         })
         .catch((error) => {
+            e.currentTarget.reset();
             // router.push('/login');
-            console.error('Error:', error);
+            // console.error('Error:', error);
         });
     }
 
 
     return (
-        <form className='input-container'>
+        <form className='input-container' onSubmit={singRequest}>
             <input name="Email or Username" type="text" className={`input ${error ? 'InputError' : ""}`}  placeholder="Email or Username"/>
             <input name="Password" type="password" autoComplete="none" className={`input ${error ? 'InputError' : ""}`}  placeholder="Password" />
-            <button id="singbtn" onClick={singRequest}>Sign in</button>
             {error && <p className="ErrorMessage">Invalid username or password</p>}
+            <button type="submit" id="singbtn">Sign in</button>
         </form>
     )
 }
