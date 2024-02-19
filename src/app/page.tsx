af8e52eda50848ac5eea8057aaaf81997b5cbfa8
essadike-elhafiane <saddik.bo@gmail.com>
2024-02-19 01:45:09 +0100

    Add: sign-in and sign-up frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from 'react';
-import '../style/landingPage.css';
+import '../styles/login/landingPage.css';
 // import BackGround from '@/components/bg';
 import axios from 'axios';
 import UserDataContext, { UserData } from '@/components/context';
