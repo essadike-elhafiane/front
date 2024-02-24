340cb3cbcfe6511284cd4bb74dfb70ee296b079e
essadike-elhafiane <saddik.bo@gmail.com>
2024-02-24 16:19:50 +0100

    Add: domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,50 @@
-"use client";
-"use strict";
+
 import { useEffect, useState } from 'react';
 import '../styles/login/landingPage.css';
 import axios from 'axios';
 import UserDataContext, { UserData } from '@/components/context';
 import Home from './home/Home';
 import { Loding } from './home/Loding';
-
 import { useRouter } from 'next/navigation';
+import cookie from 'cookie';
 
-export default function landingPage()
-{
+export default function landingPage() {
     const [data, setData] = useState<UserData | null>(null);
-    
     const router = useRouter();
 
     useEffect(() => {
-    const getdata = async () => {
+        const getdata = async () => {
             try {
-                const ApiUrl =  process.env.NEST_API;
-                // //console.log('ApiUrl:', ApiUrl, process.env);
-                //console.log('Success:', ApiUrl+ '/status');
-                const res = await axios.get(ApiUrl + '/status',
-                {
+                const ApiUrl = process.env.NEST_API;
+                const cookies = cookie.parse(document.cookie);
+                console.log(cookies.jwt);
+
+                const res = await axios.get(ApiUrl + '/status', {
                     headers: {
                         'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${cookies.jwt}`,
                     },
                     withCredentials: true,
-                }
-                );
-                if (res.data === undefined || res.data === false || res.data === null) {
-                    //console.log('Error:', res.data);
-                    router.push('/login');
-                }
-                else
-                    setData(res.data);
-                } catch (error) {
-                    //console.log('Error:', error);
-                    router.push('/login');
-                }
-        }
-        getdata();
-    },[]);
+                });
 
-    // useEffect(() => {
-    //     const jwt = window.location.hash.split('=')[1];
-    //     if (jwt) {
-    //         sessionStorage.setItem('jwt', jwt); // Consider more secure storage options
-    //         window.location.hash = '';
-    //     }
-    // }, []);
-    
+                if (res.data === undefined || res.data === false || res.data === null) {
+                    router.push('/login');
+                } else {
+                    setData(res.data);
+                }
+            } catch (error) {
+                router.push('/login');
+            }
+        };
+
+        getdata();
+    }, []);
+
     return (
         <>
             <UserDataContext.Provider value={data}>
-            {data? <Home/> : <Loding/>}
+                {data ? <Home /> : <Loding />}
             </UserDataContext.Provider>
         </>
-        );
+    );
 }
-// req.password
